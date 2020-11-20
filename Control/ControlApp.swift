5b91f921b44370aa9d110b238761577e4155ac56
Joe Blau <im@joeblau.com>
2020-11-20 15:41:44 -0800

    Initial Commit

---
+++ Control/ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlApp.swift
+//  Control
+//
+//  Created by Joe Blau on 11/20/20.
+//
+
+import SwiftUI
+
+@main
+struct ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
